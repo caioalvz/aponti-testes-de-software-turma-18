2217d4788d3733a2e630d8e2cce9089807ec1d62
Caio Alves <caioalves91@gmail.com>
2026-09-03 11:54:48 -0300

    docs: edit files structure

---
+++ 02-Teste de Software/Módulo 2/Exercicio 1/src/hello.ts
@@ -0,0 +1,7 @@
+// 1 variaveis e tipos primitivos
+
+const saudacao:string = 'Hello world!';
+let ano:number = 2026;
+
+console.log(saudacao, ano)
+
---
+++ 02-Teste de Software/Módulo 2/Exercício 1/script.ts
@@ -0,0 +1,3 @@
+let saudacao:string = "Hello World!"
+
+console.log(saudacao)
